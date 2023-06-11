5adba56cfbf074d0c2e15d93cff26f6482eaf24d
min <rlaalswl1238@naver.com>
2023-06-11 14:58:16 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {Button,Container,Nav,Navbar,Row,Col} from 'react-bootstrap';
 import './App.css';
+import data from './components/data.js'
+import { Routes, Route,Link,useNavigate,Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.jsx';
 
 function App() {
+  let [shoes] = useState(data)
+  let navigate = useNavigate()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">니고네고</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>상세페이지</Nav.Link>
+          </Nav>
+        </Container>
+      <Button variant="primary">Primary</Button>
+      </Navbar>
+
+
+      <Routes>
+        <Route path='/' element={<div>
+          <div className="container">
+        <div className="row">
+          {
+            shoes.map((a,i)=>{
+              return(
+                <Card shoes={shoes[i]} i={1}></Card>
+              )
+            })
+          }
+        </div>
+      </div>
+        </div>}  />
+        <Route path='/detail/:id' element={<Detail shoes={shoes}/>}  />
+        <Route path='*' element={<div>없는페이지요</div>}  />
+      </Routes>
+      
+
     </div>
   );
 }
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
+}
+
 
 export default App;
